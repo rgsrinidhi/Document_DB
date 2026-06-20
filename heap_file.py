98c8d8f3f78f73dffe120e8a2d71f1c836cd0d53
srinidhirgirish <srinidhirgirish@gmail.com>
2026-06-20 23:51:13 +0530

    Bug Fix - Heap File

--- heap_file.py
+++ heap_file.py
@@ -7,24 +7,37 @@ import json
 # Heap data entry format [type][length][doc_id][tombstone][data]
 
 class HeapFile: 
-    
-    def write(doc_id, data):
-        # Write a document to the heap file
-        with open('heap_file.bin', 'ab') as f:
-            f.write(json.dumps(0x01).encode('utf-8'))  # Encoding type (1 = JSON)
-            f.write(json.dumps(len(data)).encode('utf-8'))
-            f.write(json.dumps(doc_id).encode('utf-8'))
-            f.write(json.dumps(0x00).encode('utf-8'))  # Tombstone (0 = alive)
-            f.write(json.dumps(data).encode('utf-8'))
 
-    def read(offset):
-        with open('heap_file.bin', 'rb') as f:
+    def __init__(self):
+        self.heap_file = 'heap_file.bin'
+
+    def write(self, doc_id, data):
+        # Write a document to the heap file
+        
+        with open(self.heap_file, 'ab') as f:
+            f.seek(0, 2)  # Move to the end of the file
+            offset = f.tell()  # Get the current offset 
+            f.write(struct.pack('B', 0x01))  # Encoding type (1 = JSON)
+            serialised_data = json.dumps(data).encode('utf-8')
+            f.write(struct.pack('I', len(serialised_data)))
+            f.write(struct.pack('Q', doc_id))
+            f.write(struct.pack('B', 0x01))  # Tombstone (1 = alive)
+            f.write(serialised_data)
+            return offset  # Return the offset where the document was written
+            
+
+    def read(self, offset):
+        with open(self.heap_file, 'rb') as f:
             f.seek(offset)
-            encodingType = f.read(1)
-            length = f.read(4)
-            tombstone = f.read(1)
-            doc_id = f.read(4)
+            encodingType = struct.unpack('B', f.read(1))[0]
+            length = struct.unpack('I', f.read(4))[0]
+            doc_id = struct.unpack('Q', f.read(8))[0]
+            tombstone = struct.unpack('B', f.read(1))[0]
             data = f.read(length).decode('utf-8')
-            if tombstone == b'\x00':
-                return json.loads(data)
+            if tombstone == 0x01:  # If the document is not deleted
+                result = {
+                    'doc_id': doc_id,
+                    'data': json.loads(data)
+                }
+                return result
         return None
